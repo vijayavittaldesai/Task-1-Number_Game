35ad62d848297c1d91179ffc98ba2e3aa950fd20
vijayavittaldesai <vvdvijay18@gmail.com>
2023-09-17 13:53:41 +0530

    Task 1

---
+++ src/ATMInterface/example.java
@@ -0,0 +1,69 @@
+package ATMInterface;
+
+import java.util.*;
+public class example {
+    private int balance=10000;
+    public static void main(String[] args) {
+        int choice,check=1;
+
+
+        Scanner sc=new Scanner(System.in);
+        while(check==1){
+            System.out.println("Withdraw---->1\nDeposit---->2\nCheck Balance---->3\n");
+            System.out.println("Enter your choice");
+            choice=sc.nextInt();
+            example obj=new example();
+            switch(choice){
+                case 1:obj.Withdraw();
+                    break;
+                case 2:obj.Deposit();
+                    break;
+                case 3:obj.CheckBalance();
+                    break;
+                case 4:System.exit(0);
+                    break;
+                default:System.out.println("Invalid Option");
+            }
+            System.out.println("\n1---->Countinue\n0---->Exit");
+            check=sc.nextInt();
+        }
+    }
+
+    private void Withdraw() {
+        int amount=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the amount to be withdrawn");
+        amount=sc.nextInt();
+        if(balance>=amount){
+            if(amount%500==0){
+                balance=balance-amount;
+                System.out.println("Withdraw Successful");
+                System.out.println("Balance\n"+balance);
+            }
+            else
+                System.out.println("Enter amount in multiples of 500");
+        }
+        else
+            System.out.println("Insufficient Balance");
+
+    }
+
+    private void Deposit() {
+        int amount=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter amount to deposit");
+        amount=sc.nextInt();
+        if(amount%100==0){
+            balance=balance+amount;
+            System.out.println("Deposit Successful");
+            System.out.println("Balance\n"+balance);
+        }
+        else
+            System.out.println("Enter amount in multiples of 100");
+    }
+
+    private void CheckBalance() {
+        System.out.println("Balance\n"+balance);
+    }
+
+}
